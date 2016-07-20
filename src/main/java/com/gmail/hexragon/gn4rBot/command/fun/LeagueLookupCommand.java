4432ed0c2d8875dd72b97ce20afbc8b9d866951c
Maeyrl <maeyrlotg@gmail.com>
2016-07-20 14:19:36 -0600

    Added League of Legends support, re

---
+++ src/main/java/com/gmail/hexragon/gn4rBot/command/fun/LeagueLookupCommand.java
@@ -0,0 +1,97 @@
+package com.gmail.hexragon.gn4rBot.command.fun;
+
+import com.gmail.hexragon.gn4rBot.GnarBot;
+import com.gmail.hexragon.gn4rBot.managers.commands.CommandExecutor;
+import com.gmail.hexragon.gn4rBot.managers.commands.CommandManager;
+import net.dv8tion.jda.MessageBuilder;
+import net.dv8tion.jda.events.message.MessageReceivedEvent;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+public class LeagueLookupCommand extends CommandExecutor
+{
+    public LeagueLookupCommand(CommandManager manager)
+    {
+        super(manager);
+        showInHelp(true);
+    }
+
+    @Override
+    public void execute(MessageReceivedEvent event, String[] args)
+    {
+        MessageBuilder mb = new MessageBuilder();
+        try {
+            JSONObject jso = readJsonFromUrl("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"
+                    + args[0] + "?api_key=" + GnarBot.lolToken);
+            JSONObject info = (JSONObject) jso.get(args[0]);
+            mb.appendString("League Of Legends Account Info: \n")
+                    .appendString("Season: SEASON2016 \n\n")
+                    .appendString("Current Name: " + info.get("name") + "\n")
+                    .appendString("ID: " + info.get("id") + "\n")
+                    .appendString("Level: " + info.get("summonerLevel") + "\n")
+                    .appendString("Icon ID: " + info.get("profileIconId") + "\n");
+
+            try {
+                JSONObject jso2 = readJsonFromUrl(
+                        "https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/" + info.get("id")
+                                + "/summary?season=SEASON2016&api_key=RGAPI-4A230644-F1D8-40B7-A81E-4566E145BA1C");
+                JSONArray j = (JSONArray) jso2.get("playerStatSummaries");
+                JSONObject fin = (JSONObject) j.get(0);
+
+                mb.appendString("Wins: " + fin.get("wins"));
+
+                JSONObject stats = (JSONObject) fin.get("aggregatedStats");
+
+                mb.appendString("Champion Kills: " + stats.get("totalChampionKills") + "\n")
+                        .appendString("Minion Kills: " + stats.get("totalMinionKills") + "\n")
+                        .appendString("Assists: " + stats.get("totalAssists") + "\n")
+                        .appendString("Neutral Minion Kills: " + stats.get("totalNeutralMinionsKilled") + "\n")
+                        .appendString("Turrets Killed: " + stats.get("totalTurretsKilled"));
+
+                event.getChannel().sendMessage(mb.build());
+            } catch (Exception e) {
+                event.getChannel()
+                        .sendMessage("Account not found or there was an error connecting to the LoL API\n"
+                                + "Here is what I was able to get: \n");
+                try {
+                    event.getChannel().sendMessage(mb.build());
+                } catch (Exception ex) {e.printStackTrace();}
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            event.getChannel()
+                    .sendMessage("Account not found or there was an error connecting to the LoL API\n"
+                            + "Here is what I was able to get: \n");
+            try {
+                event.getChannel().sendMessage(mb.build());
+            } catch (Exception ex) { e.printStackTrace();}
+            e.printStackTrace();
+        }
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+}
